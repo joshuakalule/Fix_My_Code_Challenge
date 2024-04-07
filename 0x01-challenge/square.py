a6cf882e0f87a9cf31179b233c0da00cfdc77490
Kalule Joshua <kaljoshkalule@gmail.com>
2024-04-07 14:17:16 +0000

    correct area_of_my_square, pycodestyle, snakestyle, docs

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -11,28 +11,50 @@ class square():
         hight: int
     """
 
-    width = 0
-    height = 0
+    def __init__(self, width=0, height=0):
+        """
+        Initialize square width and height.
 
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        Attributes:
+            width: int
+            height: int
+        """
+
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
-        """Area of the square """
+        """
+        Area of the square.
+
+        Returns:
+            int: the area of the square
+        """
+
         return self.width * self.height
 
     def perimeter_of_my_square(self):
-        """Perimeter of the square."""
+        """
+        Perimeter of the square.
+
+        Returns:
+            int: perimeter of the square
+        """
+
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """String representation."""
+        """
+        String representation.
+
+        Returns:
+            str: string representation of square
+        """
+
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
